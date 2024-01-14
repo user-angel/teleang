6a29779b76bea58ef95e65047ea53ed1fe2d1ced
user-angel <122977244+user-angel@users.noreply.github.com>
2024-01-14 15:51:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => res.send('Yo boi!!'));
+
+app.listen(port, () =>
+	console.log(`Your app is listening to http://localhost:${port}`)
+);
 bot.start((ctx) => ctx.reply('Ты Лиза?')) // Команда /start отправляет сообщение с текстом "Ты Лиза?"
 
 bot.on('text', (ctx) => { // Бот ожидает сообщение
@@ -12,3 +20,4 @@ bot.on('text', (ctx) => { // Бот ожидает сообщение
 })
 
 bot.launch() // Запускаем бота
+console.log("bot ready")
